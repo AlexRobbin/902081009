f1ee631117563568f06049b55fe777395b623d17
AlexRobbin <158841503+AlexRobbin@users.noreply.github.com>
2024-02-13 09:06:30 +0330

    logical operator

---
+++ logical op.py
@@ -0,0 +1,33 @@
+#..............>=...............
+'''
+x = 2
+r = 2
+if x >= r:
+    print("x is greater than r")
+elif x <= r:
+    print("x is less than r")
+else:
+    None
+'''
+#............end...................
+
+#...........logical operator..............
+
+print(True and True)
+print(False and True)
+print(True and False)
+print(False and False)
+
+print("_________________")
+
+print(True or True)
+print(True or True)
+print(True or True)
+print(True or True)
+
+print("_________________")
+
+print(not(True))
+print(not(False))
+
+
